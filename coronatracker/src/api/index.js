a28f4e768dd82350a8c50432e56b0901f869a85c
jordan <jordan@jordans-MacBook-Air.local>
2020-04-12 11:26:47 -0700

    deconstructed objects from api

--- coronatracker/src/api/index.js
+++ coronatracker/src/api/index.js
@@ -4,8 +4,14 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
     try{
-        const response = await axios.get(url)
-        return (response);
+        const {data: {confirmed, recovered, deaths, lastUpdate}} = await axios.get(url)
+        const modifiedData = {
+            confirmed,
+            recovered,
+            deaths,
+            lastUpdate
+        }
+        return modifiedData;
     } catch (error) {
 
     }
