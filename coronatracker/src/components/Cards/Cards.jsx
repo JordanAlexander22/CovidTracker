0ee5ff4bd15e479a2272a8b05f7931abd9334a51
jordan <jordan@jordans-MacBook-Air.local>
2020-04-13 12:53:12 -0700

    impleneted material ui and started working on cards

--- coronatracker/src/components/Cards/Cards.jsx
+++ coronatracker/src/components/Cards/Cards.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import {Card, CardContent, Typography, Grid} from '@material-ui/core';
+import styles from './Cards.module.css';
+// import { StylesContext } from '@material-ui/styles';
 
-const Cards = () => {
+const Cards = (props) => {
+    console.log(props)
     return (
-        <h1>Cards component</h1>
+        <div className= {styles.container}>
+            <Grid container spacing = {3} justify = "center" >
+                <Grid item component = {Card}>
+                    <CardContent>
+                        <Typography color = "textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant= "h5">Real Data </Typography>
+                        <Typography color = "textSecondary"> Real Date </Typography>
+                        <Typography variant = "body2">Number of current Covid-19 cases</Typography> 
+                    </CardContent>
+                </Grid>
+
+            </Grid>
+        </div>
     )
 }
 
