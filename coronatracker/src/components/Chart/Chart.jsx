c5616bd06bdae910b7a19d4fb87ae85bcac0da4a
jordan <jordan@jordans-MacBook-Air.local>
2020-04-14 10:42:54 -0700

    updated dependencies and getting started on chart

--- coronatracker/src/components/Chart/Chart.jsx
+++ coronatracker/src/components/Chart/Chart.jsx
@@ -1,5 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import {fetchDailyData} from '../../api';
+import {Line, Bar} from 'react-chartjs-2';
+
+import styles from './Chart.module.css';
 
 const Chart = () => {
     const [dailyData, setdailyData] = useState({})
@@ -8,9 +11,23 @@ const Chart = () => {
         const fetchAPI = async () => {
             setdailyData(await fetchDailyData());
         }
+        
+        console.log(dailyData);
 
         fetchAPI();
     });
+
+    const lineChart = (
+       dailyData[0] 
+       ?(
+       <Line
+        data= {{
+            labels: '', 
+            datasets: [{}, {}], 
+             }} 
+       />) : null
+    );
+
     return (
         <h1>Chart component</h1>
     )
