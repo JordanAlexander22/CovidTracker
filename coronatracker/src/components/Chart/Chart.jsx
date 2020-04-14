e81ce167750275e70f9e6d610b174856fc91e3d1
jordan <jordan@jordans-MacBook-Air.local>
2020-04-14 11:19:42 -0700

    added graph and some debugging

--- coronatracker/src/components/Chart/Chart.jsx
+++ coronatracker/src/components/Chart/Chart.jsx
@@ -5,7 +5,7 @@ import {Line, Bar} from 'react-chartjs-2';
 import styles from './Chart.module.css';
 
 const Chart = () => {
-    const [dailyData, setdailyData] = useState({})
+    const [dailyData, setdailyData] = useState([])
 
     useEffect(() => {
         const fetchAPI = async () => {
@@ -18,18 +18,33 @@ const Chart = () => {
     });
 
     const lineChart = (
-       dailyData[0] 
+       dailyData.length
        ?(
        <Line
         data= {{
-            labels: '', 
-            datasets: [{}, {}], 
+            labels: dailyData.map(({date}) => date), 
+            datasets: [{
+            data: dailyData.map(({confirmed}) => confirmed),
+            label: 'Infected',
+            borderColor: '#3333ff',
+            fill: true, 
+        }, {
+            data: dailyData.map(({deaths}) => deaths),
+            label: 'Deaths',
+            borderColor: 'red',
+            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+            fill: true,
+                
+            }], 
              }} 
        />) : null
     );
 
     return (
-        <h1>Chart component</h1>
+        <div className= {styles.container}>
+            {lineChart}
+
+        </div>
     )
 }
 
