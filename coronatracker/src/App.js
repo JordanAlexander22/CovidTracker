7d2e452ca71d8092193a9e634e5c0f9d3865678d
jordan <jordan@jordans-MacBook-Air.local>
2020-04-15 10:02:45 -0700

    getting data from the country

--- coronatracker/src/App.js
+++ coronatracker/src/App.js
@@ -6,19 +6,26 @@ import {fetchData} from './api';
 
 class App extends React.Component {
   state = {
-    data: {}
+    data: {},
+    country: '',
   }
 
   async componentDidMount(){
     const fetchedData = await fetchData();
     this.setState({data: fetchedData});
   }
+
+  handleCountryChange= async (country) => {
+    const fetchedData = await fetchData(country);
+    console.log(fetchedData);
+  }
+
   render() {
     const { data } = this.state;
     return (
       <div className = {styles.container}>
         <Cards data= {data}/>
-        <CountryPicker />
+        <CountryPicker handleCountryChange= {this.handleCountryChange} />
         <Chart />
       </div>
     );
