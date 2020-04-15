20e914f54abd09734937860b1def2487533a4990
jordan <jordan@jordans-MacBook-Air.local>
2020-04-15 15:47:53 -0700

    inserted image and doing styling

--- coronatracker/src/App.js
+++ coronatracker/src/App.js
@@ -4,6 +4,8 @@ import {Cards, Chart, CountryPicker} from './components';
 import styles from './App.module.css';
 import {fetchData} from './api';
 
+import covidImage from './images/covid-19.png'
+
 class App extends React.Component {
   state = {
     data: {},
@@ -24,6 +26,7 @@ class App extends React.Component {
     const { data, country } = this.state;
     return (
       <div className = {styles.container}>
+        <img className= {styles.image} src= {covidImage} alt= "Covid-19" />
         <Cards data= {data}/>
         <CountryPicker handleCountryChange= {this.handleCountryChange} />
         <Chart data= {data} country = {country} />
